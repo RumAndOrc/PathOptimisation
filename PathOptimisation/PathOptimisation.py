3d19c1599694842b2d1f11039737d412c5bb2719
Dom <domcurran5@gmail.com>
2025-04-29 07:23:27 +1000

    Create legs, and save legs and nodes in array and dict respectively

--- PathOptimisation/PathOptimisation.py
+++ PathOptimisation/PathOptimisation.py
@@ -34,9 +34,10 @@ class PathOptimisationFrame(tk.Frame):
 
         self.canvas_map.bind("<Button-2>", self.create_node)
         self.canvas_map.tag_bind("node", "<Button-1>",self.start_create_leg)
-        #self.canvas_map.tag_bind("node", "<ButtonRelease-1>",self.end_create_leg)
+        self.canvas_map.tag_bind("node", "<ButtonRelease-1>",self.end_create_leg)
         self.leg_start = None
-        self.nodes = []
+        self.nodes = {}
+        self.legs = []
 
 
     def create_node(self, event):
@@ -45,6 +46,8 @@ class PathOptimisationFrame(tk.Frame):
                                    event.x+(NODE_SIZE/2), event.y+(NODE_SIZE/2), # type: ignore
                                    fill = NODE_INNER_COLOR, # type: ignore
                                    tags = ["node",id]))
+        self.nodes.update({id:{'x':event.x, 'y':event.y}})
+        return
     
 
     def get_next_ava_node_id(self):
@@ -66,17 +69,55 @@ class PathOptimisationFrame(tk.Frame):
         print("target node[s]:",target)
         if(len(target) == 1):
             return target[0]
-        else:
-            return None
+        return None
+
+
+    def get_leg_id(self,ids):
+        if len(ids) !=2:
+            return "leg_id_invalid"
+        num1 =int(ids[0].split('_')[-1])
+        num2 =int(ids[1].split('_')[-1])
+        if num1>num2:
+            ids == ids[::-1]
+        return ids[0]+"<->"+ids[1]
+    
 
     def start_create_leg(self, event):
         self.leg_start = self.get_node_hit_single(event.x, event.y)
+        print("start leg:",self.leg_start,self.canvas_map.gettags(self.leg_start))
+        return
+    
+
+    def get_node_id(self, node):
+        tags = self.canvas_map.gettags(node)
+        for tag in tags:
+            if "node_id_" in tag:
+                return tag
+
         
+    def end_create_leg(self, event):
+        leg_end = self.get_node_hit_single(event.x, event.y)
+        if self.leg_start != None and leg_end != self.leg_start:
+            id_start = self.get_node_id(self.leg_start)
+            id_end = self.get_node_id(leg_end)
+            leg_id = self.get_leg_id([id_start, id_end])
+
+            if leg_id in self.legs:
+                print("cautuion: leg already exists")
+                return
+            
+            self.legs.append(leg_id)
+            x1 = self.nodes[id_start]['x']
+            y1 = self.nodes[id_start]['y']
+            x2 = self.nodes[id_end]['x']
+            y2 = self.nodes[id_end]['y']
+            self.canvas_map.create_line(x1,y1, x2, y2, tags="leg_id_"+leg_id)
 
 
-    #def end_create_leg(self, event):
-        #if(self.leg_start)
-
+        print(self.legs)
+        print(self.nodes)
+        self.leg_start == None
+        return
 
 
     def toggle_settings(self, event):
@@ -87,6 +128,7 @@ class PathOptimisationFrame(tk.Frame):
             self.canvas_controls.pack_forget()
             self.canvas_settings.pack(side="right")
         self.is_settings_visible = not self.is_settings_visible
+        return
 
 
     def on_click(self, event):
@@ -94,6 +136,7 @@ class PathOptimisationFrame(tk.Frame):
         self.start_y = event.y
         print(event.x, event.y)
         self.current_shape = None
+        return
 
 
     def on_drag(self, event):
@@ -104,12 +147,14 @@ class PathOptimisationFrame(tk.Frame):
             if self.current_shape:
                 self.canvas_map.delete(self.current_shape)
             self.current_shape = self.canvas_map.create_rectangle(self.start_x, self.start_y, x, y, fill=NODE_INNER_COLOR)# type: ignore
+        return
 
 
     def on_release(self, event):
         self.start_x = None
         self.start_y = None
         self.current_shape = None
+        return
 
 
 def main():
