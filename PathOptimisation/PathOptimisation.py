6778e6eb99b15523c938c29960b1eb17cccd80a7
Dom <domcurran5@gmail.com>
2025-04-29 06:31:07 +1000

    Added create node function with unique IDs and changed to PEP-8 style

--- PathOptimisation/PathOptimisation.py
+++ PathOptimisation/PathOptimisation.py
@@ -1,45 +1,93 @@
 import tkinter as tk
-
 from PIL import Image,ImageTk 
-
 from vars import *  # type: ignore
+
 
 class PathOptimisationCanvas(tk.Canvas):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         
+
 class PathOptimisationFrame(tk.Frame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
-        self.MapCanvas = PathOptimisationCanvas(self, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg=CANVAS_BG)# type: ignore
-        self.MapCanvas.pack(side="left")
-        self.MapCanvas.bind("<Button-1>", self.on_click)
-        self.MapCanvas.bind("<B1-Motion>", self.on_drag)
-        self.MapCanvas.bind("<ButtonRelease-1>", self.on_release)
+        self.canvas_map = PathOptimisationCanvas(self, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg=CANVAS_BG)# type: ignore
+        self.canvas_map.pack(side="left")
+        #self.MapCanvas.bind("<Button-1>", self.on_click)
+        #self.MapCanvas.bind("<B1-Motion>", self.on_drag)
+        #self.MapCanvas.bind("<ButtonRelease-1>", self.on_release)
+    
+        self.is_settings_visible = False
+        self.canvas_controls = tk.Canvas(self, width=SIDEBAR_WIDTH, height = SIDEBAR_HEIGHT, bg = SIDEBAR_BG)# type: ignore
+        self.canvas_controls.create_rectangle(SIDEBAR_WIDTH-50, SIDEBAR_HEIGHT-30, SIDEBAR_WIDTH, SIDEBAR_HEIGHT, fill="red", tags ="btn_settings")# type: ignore
+        self.canvas_controls.tag_bind("btn_settings", "<Button-1>",self.toggle_settings)
+        self.canvas_controls.pack(side="right")  
 
-        self.isSettingsPanelVisible = False
-        self.ControlsCanvas = tk.Canvas(self, width=SIDEBAR_WIDTH, height = SIDEBAR_HEIGHT, bg = SIDEBAR_BG)# type: ignore
-        btn_settings = self.ControlsCanvas.create_rectangle(SIDEBAR_WIDTH-50, SIDEBAR_HEIGHT-30, SIDEBAR_WIDTH, SIDEBAR_HEIGHT, fill="red", tags ="btn_settings")# type: ignore
-        self.ControlsCanvas.tag_bind(btn_settings, "<Button-1>",self.swapToSettings)
-        self.ControlsCanvas.pack(side="right")  
-
-        self.SettingsCanvas = tk.Canvas(self, width=SIDEBAR_WIDTH, height = SIDEBAR_HEIGHT, bg = SIDEBAR_BG)# type: ignore
-        btn_Settings = self.SettingsCanvas.create_rectangle(SIDEBAR_WIDTH-50, SIDEBAR_HEIGHT-30, SIDEBAR_WIDTH, SIDEBAR_HEIGHT, fill="red", tags ="btn_settings")# type: ignore
-        self.SettingsCanvas.tag_bind(btn_settings, "<Button-1>",self.swapToSettings)
-        self.SettingsCanvas.create_text(SIDEBAR_WIDTH/2, 50, text="Settings") # type: ignore
+        self.canvas_settings = tk.Canvas(self, width=SIDEBAR_WIDTH, height = SIDEBAR_HEIGHT, bg = SIDEBAR_BG)# type: ignore
+        self.canvas_settings.create_rectangle(SIDEBAR_WIDTH-50, SIDEBAR_HEIGHT-30, SIDEBAR_WIDTH, SIDEBAR_HEIGHT, fill="red", tags ="btn_settings")# type: ignore
+        self.canvas_settings.tag_bind("btn_settings", "<Button-1>",self.toggle_settings)
+        self.canvas_settings.create_text(SIDEBAR_WIDTH/2, 50, text="Settings") # type: ignore
 
         self.start_x = None
         self.start_y = None
         self.current_shape = None
 
-    def swapToSettings(self, event):
-        if(self.isSettingsPanelVisible):
-            self.SettingsCanvas.pack_forget()
-            self.ControlsCanvas.pack(side="right")
+        self.canvas_map.bind("<Button-2>", self.create_node)
+        self.canvas_map.tag_bind("node", "<Button-1>",self.start_create_leg)
+        #self.canvas_map.tag_bind("node", "<ButtonRelease-1>",self.end_create_leg)
+        self.leg_start = None
+        self.nodes = []
+
+
+    def create_node(self, event):
+        id = self.get_next_ava_node_id()
+        print("Created:",self.canvas_map.create_oval(event.x-(NODE_SIZE/2), event.y-(NODE_SIZE/2), # type: ignore
+                                   event.x+(NODE_SIZE/2), event.y+(NODE_SIZE/2), # type: ignore
+                                   fill = NODE_INNER_COLOR, # type: ignore
+                                   tags = ["node",id]))
+    
+
+    def get_next_ava_node_id(self):
+        valid_id=None
+        id = 0
+        while(valid_id == None):
+            to_try = "node_id_"+str(id)
+            if(len(self.canvas_map.find_withtag(to_try))==0):
+                valid_id = to_try
+            else:
+                id+=1
+        return valid_id
+    
+
+    def get_node_hit_single(self, x,y):
+        all_nodes = self.canvas_map.find_withtag("node")
+        hits = self.canvas_map.find_overlapping(x-1, y-1, x+1, y+1)
+        target = list(set(all_nodes)&set(hits))
+        print("target node[s]:",target)
+        if(len(target) == 1):
+            return target[0]
         else:
-            self.ControlsCanvas.pack_forget()
-            self.SettingsCanvas.pack(side="right")
-        self.isSettingsPanelVisible = not self.isSettingsPanelVisible
+            return None
+
+    def start_create_leg(self, event):
+        self.leg_start = self.get_node_hit_single(event.x, event.y)
+        
+
+
+    #def end_create_leg(self, event):
+        #if(self.leg_start)
+
+
+
+    def toggle_settings(self, event):
+        if(self.is_settings_visible):
+            self.canvas_settings.pack_forget()
+            self.canvas_controls.pack(side="right")
+        else:
+            self.canvas_controls.pack_forget()
+            self.canvas_settings.pack(side="right")
+        self.is_settings_visible = not self.is_settings_visible
+
 
     def on_click(self, event):
         self.start_x = event.x
@@ -47,27 +95,28 @@ class PathOptimisationFrame(tk.Frame):
         print(event.x, event.y)
         self.current_shape = None
 
+
     def on_drag(self, event):
         if self.start_x is not None and self.start_y is not None:
             x = event.x
             y = event.y
 
             if self.current_shape:
-                self.MapCanvas.delete(self.current_shape)
-            self.current_shape = self.MapCanvas.create_rectangle(self.start_x, self.start_y, x, y, fill=NODE_INNER_COLOR)# type: ignore
+                self.canvas_map.delete(self.current_shape)
+            self.current_shape = self.canvas_map.create_rectangle(self.start_x, self.start_y, x, y, fill=NODE_INNER_COLOR)# type: ignore
+
 
     def on_release(self, event):
         self.start_x = None
         self.start_y = None
         self.current_shape = None
 
+
 def main():
     root = tk.Tk()
     root.title("Custom Canvas Example")
     TopFrame = PathOptimisationFrame(root, width=CANVAS_WIDTH+SIDEBAR_WIDTH, height=CANVAS_HEIGHT, bg="yellow")# type: ignore
-    
     TopFrame.pack(padx=0, pady=0)
-
     root.mainloop()
 
 
@@ -75,7 +124,7 @@ def main():
     TODO
     For updating settings to 'vars.py' file
 '''
-def updateSettingsFile():
+def update_settings_file():
     file = open("vars.py", 'r')
     for line in file:
         print(line)
@@ -89,6 +138,6 @@ def updateSettingsFile():
     """
     file.close
 
+
 if __name__ == "__main__":
-    
     main()
